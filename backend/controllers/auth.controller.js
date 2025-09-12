54da7c1bd2eaac646d19d98c9d63fb06223fa843
Shubham Prajapati <155132712+SHUBSHIV@users.noreply.github.com>
2025-09-13 00:56:37 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -35,7 +35,7 @@ export const signup = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: "strict",
+            sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
         });
 
@@ -73,7 +73,7 @@ export const signin = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: "strict",
+            sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
         });
 
